64877c1f9d77cce3b2c056dbc514129f76c6c9e2
DavMateo <145727671+DavMateo@users.noreply.github.com>
2023-09-30 03:35:12 -0500

    .

--- Problemas de guias anteriores/Guia 3/calculadoraIndiceMasaCorporal.py
+++ Problemas de guias anteriores/Guia 3/calculadoraIndiceMasaCorporal.py
@@ -13,4 +13,3 @@ alturaPulgadas = float(input("Ingrese su altura en pulgadas: "))
 # CONVIRTIENDO LOS VALORES DE LIBRAS Y PULGADAS A KG Y MTS
 pesoKilogramos = pesoLibras * 0.45359237
 alturaMetros = alturaPulgadas * 0.0254
-
