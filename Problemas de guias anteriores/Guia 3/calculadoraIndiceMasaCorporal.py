affb62e482ad9a4f1c0c9ba5e664a35fb8a30db5
DavMateo <145727671+DavMateo@users.noreply.github.com>
2023-09-30 03:34:49 -0500

    .

--- Problemas de guias anteriores/Guia 3/calculadoraIndiceMasaCorporal.py
+++ Problemas de guias anteriores/Guia 3/calculadoraIndiceMasaCorporal.py
@@ -3,4 +3,14 @@
 # de la misma.
 
 
+# IMPORTAR LAS LIBRERÍAS PERTINENTES
+import math as Math
+
 # DEFINIENDO LAS VARIABLES PRINCIPALES
+pesoLibras = float(input("Ingrese su peso en libras: "))
+alturaPulgadas = float(input("Ingrese su altura en pulgadas: "))
+
+# CONVIRTIENDO LOS VALORES DE LIBRAS Y PULGADAS A KG Y MTS
+pesoKilogramos = pesoLibras * 0.45359237
+alturaMetros = alturaPulgadas * 0.0254
+
