094cb228db7dee98d66d9c015c35ef269c8ac40b
DavMateo <145727671+DavMateo@users.noreply.github.com>
2023-10-06 14:00:29 -0500

    .

--- Actividad 9/ejercicioListas.py
+++ Actividad 9/ejercicioListas.py
@@ -1,3 +1,6 @@
+# Ta difícil jefe :(
+
+
 # Este programa tiene como objetivo la gestión de la nómina de empleados para la empresa
 # ACME mediante una serie de requerimientos que la misma empresa proporcionó.
 
@@ -5,7 +8,8 @@
 
 # DEFINIENDO LAS VARIABLES PRINCIPALES
 isVerdadero = True
-listaNombreFiltrado = []  #Esta variable pertenece a la función "validarNombre"
+listaNombreFiltrado = []  #Esta variable pertenece a la función "validarNombre".
+listaEmpleados = []  #Esta es la lista principal donde se almacena la info de los empleados.
 
 
 # DEFINIENDO LAS FUNCIONES DE VALIDACIÓN DE DATOS
@@ -36,6 +40,14 @@ def validarNombre(msj):
                 if nombreArray[i] != "":
                     listaNombreFiltrado.append(nombreArray[i])
             
+            nombreFiltrado = "".join(listaNombreFiltrado).lower()
+            nombreFinal = " ".join(listaNombreFiltrado).title()
+            
+            # Validación
+            if len(listaNombreFiltrado) < 2 or not nombreFiltrado.isalnum() or len(nombreFiltrado) == 0:
+                print("Error: Debes introducir un nombre válido con al menos un nombre y un apellido.")
+                continue
+            return nombreFinal
         
         except Exception as e:
             print(f"Ha ocurrido un problema al ingresar el nombre. Error: {e}")
@@ -44,15 +56,46 @@ def validarNombre(msj):
 
 
 def validarHorasTrabajadas(msj):
-    pass
+    while True:
+        try:
+            horasTrabajadas = int(input(msj))
+            
+            if horasTrabajadas < 1:
+                print("Error: Debes ingresar un valor numérico entero igual o superior a 1.")
+                continue  
+            elif horasTrabajadas > 160:
+                print("Error: Debes ingresar un valor numérico entero igual o menor a 160.")
+                continue
+            return horasTrabajadas
+        
+        except ValueError:
+            print("Ha ocurrido un error al ingresar las horas trabajadas. Inténtelo de nuevo.")
+        except:
+            print("Ha ocurrido un error inesperado. Inténtelo de nuevo o comuníquese con un administrador.")
 
 
 def validarValorHora(msj):
-    pass
+    while True:
+        try:
+            valorHora = int(input(msj))
+            
+            if valorHora < 8000:
+                print("Error: El valor de la hora no puede ser inferior a $8,000 COP.")
+                continue
+            elif valorHora > 150000:
+                print("Error: El valor de la hora no puede ser superior a $150,000 COP.")
+                continue
+            return valorHora
+        
+        except ValueError:
+            print("Ha ocurrido un error al ingresar el valor de la hora. Inténtelo de nuevo.")
+        except:
+            print("Ha ocurrido un error inesperado. Inténtelo de nuevo o comuníquese con un administrador.")
 
 
 # DEFINIENDO LAS FUNCIONES COMPLEMENTARIAS A LAS FUNCIONES DE LOS ENUNCIADOS
-
+def reiniciarListas():
+    pass
 
 
 # DEFINIENDO LAS FUNCIONES DE LOS ENUNCIADOS
@@ -77,7 +120,6 @@ def menu():
             if opcionUsuario < 1:
                 print("Error: No puedes ingresar un número menor que 1 ni números negativos, solo entre 1-8")
                 continue
-            
             elif opcionUsuario > 8:
                 print("Error: No puedes ingresar un número mayor que 8. Ingrese un número entre 1-8")
                 continue
--- test.py
+++ test.py
@@ -1,12 +1,15 @@
-empleados = [["David Mateo", 51155, 43, 100000], ["Juan Gabriel", 5285024, 85, 135500]]
-otrosEmpleados = [["David Mateo", 5525259, 59, 100000], ["Ian Alejandro", 482526515, 78, 89511]]
+nombre = "      DavID                     maTeo            ".strip()
+nombreArray = nombre.split(" ")
+listaNombreFiltrado = []
 
+for i in range(len(nombreArray)):
+    if nombreArray[i] != "":
+        listaNombreFiltrado.append(nombreArray[i])
 
-def test():
-    for i in range(len(empleados)):
-        for j in range(0, len(empleados[i])):
-            empleados[i][j] = otrosEmpleados[i][j]
-    return empleados
-            
-print(empleados)
-print(test())
+nombreValidarFiltrado = "".join(listaNombreFiltrado).lower()
+nombreFinal = " ".join(listaNombreFiltrado).title()
+
+print(nombreArray)
+print(listaNombreFiltrado)
+print(nombreValidarFiltrado)
+print(nombreFinal)
