45d75f70f3669601ac62c08e0d7fb5e0e44c8176
DavMateo <145727671+DavMateo@users.noreply.github.com>
2023-10-07 22:35:47 -0500

    .

---
+++ 01. Problemas de guias anteriores/Guia 7 - Funciones/Ejercicios 2/funcionesPractica.py
@@ -0,0 +1,129 @@
+# Este programa contiene tres soluciones a ejercicios individuales que podrán ser
+# accedidos mediante el uso de funciones, donde el usuario elejirá en un menú que 
+# solución desea ejecutar.
+
+
+# DEFINIR LAS VARIABLES PRINCIPALES
+isVerdadero = True
+
+
+# DECLARANDO LAS FUNCIONES COMPLEMENTARIAS
+def factorial(num):
+    multiplicar = 1
+    
+    for i in range(1, int(num) + 1):
+        multiplicar *= i
+    
+    return multiplicar
+
+
+# DEFINIENDO LAS FUNCIONES DE VALIDACIÓN
+def validarOpcionUsuario(num, min, max):
+    while True:
+        try:
+            opcion = int(input(num))
+            
+            if opcion < int(min) or opcion > int(max):
+                print(f"Error: Introduce una opción válida ({min}-{max}).\n")
+                continue
+            return opcion
+        
+        except ValueError:
+            print("Ha ocurrido un error al ingresar la opción elejida. Inténtelo de nuevo.\n")
+        except:
+            print("")
+
+def validarNumero(num, min, max):
+    while True:
+        try:
+            numero = int(input(num))
+            
+            if numero < min:
+                print(f"Error: No puedes elegir un valor menor a {min}.\n")
+                continue
+            
+            # "max" será negativo si es "False", será "True" si contiene algún valor.
+            if max:
+                if numero > max:
+                    print(f"Error: No puedes elegir un valor mayor a {max}.\n")
+                    continue
+
+            return numero
+        
+        except ValueError:
+            print("Ha ocurrido un error al ingresar la opción elejida. Inténtelo de nuevo.\n")
+        except:
+            print("Ha ocurrido un error inesperado. Inténtelo de nuevo o comuníquese con un administrador.\n")
+
+def validarTexto(text, min):
+    while True:
+        try:
+            texto = input(text).strip()
+            
+            if len(texto) == min or not texto.isalnum() or texto.isdigit():
+                print("Error: El texto NO puede estar vacía, tener caracteres especiales o solo números.")
+                continue
+            return texto
+        
+        except Exception as e:
+            print("Ha ocurrido un problema al momento de ingresar la cadena de texto.\n")
+            print(f"Error: {e}")
+        except:
+            print("Ha ocurrido un error inesperado. Inténtelo de nuevo o comuníquese con un administrador.\n")
+
+
+# DEFINIENDO LAS FUNCIONES IMPORTANTES
+def menu(msj):
+    print("\n", "======== MENU ========")
+    print("\n1. Cálculo de la combinatoria")
+    print("2. Convertir texto a número")
+    print("3. Calcular el IVA de una factura")
+    print("4. Salir")
+    return validarOpcionUsuario(msj, 1, 4)
+
+def calculoCombinatoria(num1, num2):
+    print("\n", "*** CÁLCULO DE LA COMBINATORIA ***")
+    
+    n = validarNumero(num1, 1, False)
+    k = validarNumero(num2, 1, n)
+    # El objetivo del "False" al invocar "validarNumero()" para la variable "n" es para que cuando se
+    # ingrese el valor de "k", se asegure el programa que ese valor no supera a "n". Ese condicional
+    # está dentro de otro condicional cuya validación es el valor de "False"
+    
+    return (factorial(n)) / (factorial(k) * (factorial(n-k)))
+
+def textoNumero(msj):
+    texto = validarTexto(msj, 0)
+    textoArray = texto.split(" ")
+    textoArrayFiltrada = []
+    
+    for i in range(len(textoArray)):
+        if textoArray[i] != "":
+            textoArrayFiltrada.append(textoArray[i])
+    
+    textoValidar = "".join(textoArrayFiltrada)
+    textoFinal = " ".join()
+
+def IvaFactura(msj):
+    pass
+
+
+# CREANDO LA ESTRUCTURA DEL PROGRAMA
+while isVerdadero:
+    opcionUsuario = menu("   >> Ingrese una opción (1-4): ")
+    
+    if opcionUsuario == 1:
+        combinatoria = calculoCombinatoria("Ingrese el número total de elementos: ", "Ingrese el número de elementos por grupo: ")
+        
+        print(f"\nPuedes realizar: C(n,k) = {combinatoria:.0f} combinaciones distintas.")
+        input("Presione cualquier tecla para continuar...")
+    
+    elif opcionUsuario == 2:
+        pass
+    
+    elif opcionUsuario == 3:
+        pass
+    
+    elif opcionUsuario == 4:
+        print("\nSaliendo...")
+        isVerdadero = False
