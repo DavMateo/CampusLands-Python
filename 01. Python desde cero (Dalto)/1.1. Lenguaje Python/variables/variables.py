f93e82645f9bc8c3c0a3549e457bf48a656b90f6
DavMateo <145727671+DavMateo@users.noreply.github.com>
2023-10-10 12:18:11 -0500

    feat: :tada: Inicio del curso Python desde cero con Dalto

---
+++ 01. Python desde cero (Dalto)/1.1. Lenguaje Python/variables/variables.py
@@ -0,0 +1,19 @@
+# Las variables es información que se almacena en la memoria del equipo.
+
+#Definiendo una variable con camelCase
+nombreCompletoDeTuTioMasterPython = "Lucas Dalto"
+
+#Definiendo una variable con snake_case
+nombre_completo_de_tu_tio_master_python = "Lucas Dalto"
+
+
+#Concatenar con +
+bienvenida = "Hola " + " ¿Cómo estás?"
+
+#Concatenar con f-strings
+bienvenida = f"Hola {nombre_completo_de_tu_tio_master_python}. ¿Cómo estás?"
+
+
+#Operadores de pertenencia (in / not in)
+print("Lucas" in bienvenida) #True
+print("Lucas" not in bienvenida) #False
