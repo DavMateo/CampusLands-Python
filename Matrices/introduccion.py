205b9e83c20e36a2d12f94e93011b5465a42047a
DavMateo <145727671+DavMateo@users.noreply.github.com>
2023-10-09 21:29:43 -0500

    feat: :sparkles: Nuevo Tema: Matrices
    Visto nuevo tema referente a las matrices. Fundamentos básicos, no ejercicios

---
+++ Matrices/introduccion.py
@@ -0,0 +1,13 @@
+matriz = [
+    [2, 4, 6],
+    [8, 10, 12],
+    [14, 16, 18]
+]
+
+print(matriz[2][1])
+
+
+# Las matrices de igual cantidad de filas y columnas se les conoce como "Matrices cuadradas".
+
+# En Python se le puede ver a una matiz como un conjunto de listas diferentes, donde una 
+# fila representaría una lista.
