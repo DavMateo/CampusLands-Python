edae8be21e553a8c03d495db23b45ac513b5cbd5
DavMateo <145727671+DavMateo@users.noreply.github.com>
2023-10-14 11:19:03 -0500

    .

--- 01. Problemas de guias anteriores/Guia 9 - Diccionarios/diccionarios-NominaACME.py
+++ 01. Problemas de guias anteriores/Guia 9 - Diccionarios/diccionarios-NominaACME.py
@@ -11,7 +11,15 @@ subsidioTransporte = 140606
 
 
 # DEFINIENDO LAS FUNCIONES COMPLEMENTARIAS
-
+def filtrarTexto(texto):
+    textoArray = texto.split(" ")
+    textoFiltradoArray = []
+    
+    for i in range(len(textoArray)):
+        if textoArray[i] != "":
+            textoFiltradoArray.append(textoArray[i])
+    
+    return textoFiltradoArray
 
 
 # DEFINIENDO LAS FUNCIONES DE VALIDACIÓN
@@ -21,30 +29,84 @@ def validarOpcionUsuario(msj, min, max):
             opcionUsuario = int(input(msj))
             
             if opcionUsuario < min or opcionUsuario > max:
-                print(f"Error: Debes elegir una opción dentro del rango válido ({min}-{max}).")
+                print(f"Error: Debes elegir una opción dentro del rango válido ({min}-{max}).\n")
                 continue
             return opcionUsuario
         
         except ValueError:
-            print("Ha ocurrido un error al ingresar la opción elegida. Inténtelo de nuevo.")
+            print("Ha ocurrido un error al ingresar la opción elegida. Inténtelo de nuevo.\n")
         except:
-            print("Ha ocurrido un error inesperado. Inténtelo de nuevo o comuníquese con un administrador.")
-
+            print("Ha ocurrido un error inesperado. Inténtelo de nuevo o comuníquese con un administrador.\n")
 
 def validarId(msj, min):
-    pass
-
+    while True:
+        try:
+            id = int(input(msj))
+            
+            if id < min:
+                print(f"Error: El ID no puede ser menor que {min}.\n")
+                continue
+            return id
+        
+        except ValueError:
+            print("Ha ocurrido un error al ingresar el ID del empleado. Inténtelo de nuevo.\n")
+        except:
+            print("Ha ocurrido un error inesperado. Inténtelo de nuevo o comuníquese con un administrador.\n")
 
 def validarNombre(msj, min):
-    pass
-
+    while True:
+        try:
+            nombre = input(msj).strip()
+            nombreFiltradoArray = filtrarTexto(nombre)
+            
+            nombreValidar = "".join(nombreFiltradoArray)
+            nombreFinal = " ".join(nombreFiltradoArray)
+            
+            if len(nombreFiltradoArray) < min:
+                print("Error: Debes ingresar al menos un nombre y un apellido.\n")
+                continue
+            
+            elif nombreValidar.isdigit() or not nombreValidar.isalnum() or len(nombreValidar) == 0:
+                print("Error: El nombre no debe tener números ni caracteres especiales, solo letras.\n")
+                continue
+            
+            return nombreFinal
+        
+        except Exception as e:
+            print("Ha ocurrido un problema al ingresar el nombre del empleado.\n")
+            print(f"Error: {e}\n")
+        except:
+            print("Ha ocurrido un error inesperado. Inténtelo de nuevo o comuníquese con un administrador.\n")
 
 def validarHorasTrabajadas(msj, min, max):
-    pass
-
+    while True:
+        try:
+            horasTrabajadas = int(input(msj))
+            
+            if horasTrabajadas < min or horasTrabajadas > max:
+                print(f"Error: Debes ingresar un valor numérico entre el rango válido ({min}-{max}).\n")
+                continue
+            return horasTrabajadas
+        
+        except ValueError:
+            print("Ha ocurrido un error al ingresar las horas laboradas. Inténtelo de nuevo.\n")
+        except:
+            print("Ha ocurrido un error inesperado. Inténtelo de nuevo o comuníquese con un administrador.\n")
 
 def validarValorHora(msj, min, max):
-    pass
+    while True:
+        try:
+            valorHora = int(input(msj))
+            
+            if valorHora < min or valorHora > max:
+                print(f"Error: Debes ingresar un valor dentro del rango permitido (${min} COP - ${max} COP).")
+                continue
+            return valorHora
+        
+        except ValueError:
+            print("Ha ocurrido un error al ingresar el valor de la hora laboral. Inténtelo de nuevo.\n")
+        except:
+            print("Ha ocurrido un error inesperado. Inténtelo de nuevo o comuníquese con un administrador.\n")
 
 
 # DEFINIENDO LAS FUNCIONES PRINCIPALES
@@ -63,7 +125,6 @@ def menu(msj):
     print("8. Salir")
     return validarOpcionUsuario(msj, 1, 8)
 
-
 def agregarEmpleado():
     print("\n\n", "=== AGREGAR EMPLEADO ===".center(10), "\n")
     print("Para crear el nuevo empleado, se necesita de la siguiente información:")
@@ -73,26 +134,20 @@ def agregarEmpleado():
     horasTrabajadas = validarHorasTrabajadas(">> Horas trabajas: ", 1, 160)
     valorHora = validarValorHora(">> Valor de la hora: ", 8000, 150000)
 
-
 def modificarEmpleado():
     pass
-
 
 def buscarEmpleado():
     pass
 
-
 def eliminarEmpleado():
     pass
-
 
 def listarEmpleados():
     pass
 
-
 def listarNominaEmpleado():
     pass
-
 
 def listarNominasTotalEmpleados():
     pass
@@ -103,7 +158,7 @@ while isVerdadero:
     opcionUsuario = menu("   >> Escoja una opción (1-8)?: ")
     
     if opcionUsuario == 1:
-        pass
+        agregarEmpleado()
     
     elif opcionUsuario == 2:
         pass
