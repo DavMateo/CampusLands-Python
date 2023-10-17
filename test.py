8149a9b2ba3b62434194b0ba33b6a7e555b210be
DavMateo <145727671+DavMateo@users.noreply.github.com>
2023-10-16 21:08:06 -0500

    feat: :sparkles: Guía n°10 solucionado
    Guía n°10 solucionado

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
-miDiccionario = {1099737474: {"nombre": "David Mateo", "hrsLaboradas": 160, "valorhrs": 150000}}
+def calcular_serie_alterna(n):
+    suma = 0
+    signo = 1  # Para alternar entre sumar y restar
+    
+    for i in range(1, n + 1):
+        termino = signo * (1 / i)
+        suma += termino
+        signo *= -1  # Cambia el signo para el siguiente término
+    
+    return suma
 
-# test = miDiccionario.pop(1099737474)
-
-# print(test)
-# print(1099737474 in miDiccionario)
-
-print(len(miDiccionario))
-print(miDiccionario[1099737474]["hrsLaboradas"])
-print(type(miDiccionario[1099737474]["hrsLaboradas"]))
+n = int(input("Ingrese el valor de 'n': "))
+resultado = calcular_serie_alterna(n)
+print(f"La suma de la serie hasta 1/{n} es: {resultado}")
