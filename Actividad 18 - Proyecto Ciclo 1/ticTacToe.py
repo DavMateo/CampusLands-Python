c0d593f0c3c9dd39e14ef5e91a006c50234e4f5d
DavMateo <145727671+DavMateo@users.noreply.github.com>
2023-10-18 15:43:08 -0500

    feat: :tada: Inicio del proyecto Tic Tac Toe en Python
    Inicio del proyecto Tic Tac Toe en Python

---
+++ Actividad 18 - Proyecto Ciclo 1/ticTacToe.py
@@ -0,0 +1,22 @@
+# Este programa es un videojuego del reconocido Tic Tac Toe, ejecutado 100% en consola. 
+# Juego diseñado para dos jugadores, que mediante entrada de texto en consola, se elegirá
+# donde colocar la ficha en el tablero de 3x3.
+
+
+# DECLARANDO LAS VARIABLES PRINCIPALES
+
+
+
+# DEFINIENDO LAS FUNCIONES COMPLEMENTARIAS
+
+
+
+# DEFINIENDO LAS FUNCIONES DE VALIDACIÓN
+
+
+
+# DEFINIENDO LAS FUNCIONES PRINCIPALES
+
+
+
+# CREANDO LA ESTRUCTURA DEL PROGRAMA
--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print(dir({}))
+tablero = [ "-", "-", "-",
+            "-", "-", "-",
+            "-", "-", "-"]
+
+
+
+
+def verTablero():
+    print("\n")
+    print(tablero[0] + " | " + tablero[1] + " | " + tablero[2])
+    print(tablero[3] + " | " + tablero[4] + " | " + tablero[5])
+    print(tablero[6] + " | " + tablero[7] + " | " + tablero[8])
+    print("\n")
+
+verTablero()
